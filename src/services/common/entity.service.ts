412ed445afd267196d94d8938d2544f8e492f13a
18161518841tb <18161518841tb@sina.com>
2019-12-08 18:18:49 +0800

    加入事务处理

--- src/services/common/entity.service.ts
+++ src/services/common/entity.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Connection } from 'typeorm';
+import { Connection, QueryRunner } from 'typeorm';
 import { User } from '../../model/user.entity';
 
 @Injectable()
@@ -7,6 +7,23 @@ export class EntityService {
   constructor(
     private readonly connection: Connection
   ) {}
+  // 事务处理回调
+  async transaction<T = any>(cb: (query: QueryRunner) => T): Promise<T> {
+    const query = this.connection.createQueryRunner()
+    await query.connect()
+    await query.startTransaction()
+    let res:T = null
+    try {
+      res = await cb(query)
+      await query.commitTransaction()
+    } catch (e) {
+      await query.rollbackTransaction()
+      throw e
+    } finally {
+      await query.release()
+    }
+    return res
+  }
   get user() {
     return this.connection.getRepository(User)
   }
